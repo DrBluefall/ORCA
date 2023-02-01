35502e9ef2ff182305328d911a3f564d20a8b15f
Alexander Bisono <sbisonol@gmail.com>
2023-01-31 19:54:44 -0500

    Actually update on a logout

--- webserver/frontend/src/components/Login.svelte
+++ webserver/frontend/src/components/Login.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
     import { getCookie, removeCookie } from "typescript-cookie";
     import { current_user } from "../lib/userstore";
-    import { get } from "svelte/store";
 
     let cookie = getCookie("discord_user");
     if (cookie !== undefined) {
-        current_user.update((_) => JSON.parse(cookie));
+        $current_user = JSON.parse(cookie);
     }
 
     function on_logout() {
         removeCookie("discord_user");
-        current_user.update((_) => null);
+        $current_user = null;
+        window.location.href = "/";
     }
 
     function on_login() {
@@ -18,11 +18,19 @@
     }
 </script>
 
-<main>
-    {#if get(current_user) !== null}
-        Hello, {get(current_user).username}.
+{@debug current_user}
+
+<div id="login">
+    {#if $current_user !== null}
+        Hello, {$current_user.username}.
         <button on:click={on_logout}>Logout</button>
     {:else}
         <button on:click={on_login}>Login</button>
     {/if}
-</main>
+</div>
+
+<style>
+ #login {
+   float: left;
+ }
+</style>
