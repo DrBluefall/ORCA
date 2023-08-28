aff432e2b823768a7fc8493571a4308a8ff29941
Alexander Bisono <sbisonol@gmail.com>
2023-08-28 00:49:32 -0400

    Add an extra route

--- web/backend/src/routes/auth.rs
+++ web/backend/src/routes/auth.rs
@@ -4,3 +4,8 @@ use actix_web::{Responder, HttpResponse};
 pub async fn signin() -> impl Responder {
     HttpResponse::NotImplemented().finish()
 }
+
+#[actix_web::post("/signup")]
+pub async fn signup() -> impl Responder {
+    HttpResponse::NotImplemented().finish()
+}
