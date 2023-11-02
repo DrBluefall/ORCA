100f7bade37658878c8a1382bc9b16773822406b
Alexander Bisono <sbisonol@gmail.com>
2023-11-01 22:17:32 -0400

    Swap buttons on login page for SwapTextButtons

--- web/frontend/src/components/Login.svelte
+++ web/frontend/src/components/Login.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+    import SwapTextButton from "../util/SwapTextButton.svelte";
+    import SwapTextLink from "../util/SwapTextLink.svelte";
     let email = "";
     let password = "";
+    let errors: string[] = [];
 
     async function auth() {
+        errors = [];
         let response = await fetch("/api/site/signin", {
             method: "POST",
             mode: "same-origin",
@@ -10,8 +14,12 @@
             headers: { "Content-Type": "application/json" },
         });
 
-        if (response.status != 200) {
-            console.log(JSON.stringify(response));
+        if (!response.ok) {
+            if (response.status === 404) {
+                errors = [...errors, "No account exists with that email."];
+            } else if (response.status === 401) {
+                errors = [...errors, "The password is incorrect."];
+            }
         } else {
             // log in on successful auth
             window.location.replace("/");
@@ -21,6 +29,13 @@
 
 <div class="form">
     <form>
+        {#if errors.length != 0}
+            <ul id="errors">
+                {#each errors as error}
+                    <li>{error}</li>
+                {/each}
+            </ul>
+        {/if}
         <label for="email">Email</label>
         <input
             id="email"
@@ -30,23 +45,22 @@
             name="email"
             autocomplete="username"
             bind:value={email}
-            placeholder="user@example.com"
-        /><br />
+            placeholder="user@example.com" /><br />
         <label for="password">Password</label>
         <input
             id="password"
             class="input-field"
             bind:value={password}
             type="password"
-            placeholder="doNotUseThisPassword"
-        /><br />
-        <div id="signin_button">
-            <button on:click|preventDefault={async () => await auth()}
-                >Sign In</button
-            >
-            <br />
-        </div>
-        <a href="/#/signup">Don't have an account?</a>
+            placeholder="doNotUseThisPassword" />
+        <SwapTextButton
+            initial_text="Sign In"
+            swap_text="Welcome back."
+            callback={async () => await auth()} />
+        <SwapTextLink
+            dest="/#/signup"
+            initial_text="Don't have an account?"
+            swap_text="Let's fix that." />
     </form>
 </div>
 
@@ -54,27 +68,28 @@
     * {
         box-sizing: border-box;
     }
+
     div.form {
         display: block;
         text-align: center;
     }
+
     .input-field {
         padding: 5px;
         margin: 5px;
     }
+
     form {
         display: flex;
         align-items: center;
         flex-direction: column;
         box-sizing: border-box;
     }
-    button {
-        font-family: inherit;
-        font-size: 1em;
-    }
-    #signin_button {
-        width: 100%;
-        display: block;
-        padding: 10px;
+
+    #errors {
+        border: 1px solid #dd0000;
+        border-radius: 7px;
+        padding: 7px;
+        list-style-type: none;
     }
 </style>
